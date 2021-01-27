5a4690711bb04e3a6bfa062abb99534fe33d3074
Stijn Van Braband <r0740656@student.thomasmore.be>
2021-01-27 11:40:53 +0100

    Sigfox

---
+++ Project4.0_BackEnd/Controllers/SigfoxController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project4._0_BackEnd.Data;
+using Project4._0_BackEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project4._0_BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SigfoxController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public SigfoxController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task PostMeasurement(string value)
+        {
+            string[] list = value.Split(',');
+            Box box = _context.Boxes.FirstOrDefault(b => b.MacAddress == list[0]);
+            if (box == null)
+            {
+                return;
+            }
+            Measurement measurement1 = new Measurement();
+            measurement1.BoxID = box.BoxID;
+            measurement1.SensorID = 12;
+            measurement1.Value = list[1];
+            measurement1.Timestamp = DateTime.Now;
+            _context.Measurements.Add(measurement1);
+            await _context.SaveChangesAsync();
+
+        }
+    }
+}
