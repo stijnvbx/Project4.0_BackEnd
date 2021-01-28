244d0070596bad50ee1092c68df5ee1286c5153b
Stijn Van Braband <r0740656@student.thomasmore.be>
2021-01-28 13:41:14 +0100

    Sigfox

--- Project4.0_BackEnd/Controllers/SigfoxController.cs
+++ Project4.0_BackEnd/Controllers/SigfoxController.cs
@@ -20,10 +20,9 @@ namespace Project4._0_BackEnd.Controllers
             _context = context;
         }
 
-        [HttpPost("{Mac}/{Value}")]
-        public async Task PostMeasurement(string Mac, [FromBody] string Value)
+        [HttpPost("{Mac}/{tempMcu}/{tempB}/{temp}/{vocht}/{bVocht}")]
+        public async Task PostMeasurement(string Mac, string tempMcu, string tempB, string temp, string vocht, string bVocht)
         {
-            string[] list = Value.Split(",");
             Box box = _context.Boxes.FirstOrDefault(b => b.MacAddress == Mac);
             if (box == null)
             {
@@ -32,15 +31,33 @@ namespace Project4._0_BackEnd.Controllers
             Measurement measurement1 = new Measurement();
             measurement1.BoxID = box.BoxID;
             measurement1.SensorID = 12;
-            measurement1.Value = list[0];
+            measurement1.Value = tempMcu;
             measurement1.TimeStamp = DateTime.Now;
             _context.Measurements.Add(measurement1);
             Measurement measurement2 = new Measurement();
             measurement2.BoxID = box.BoxID;
             measurement2.SensorID = 11;
-            measurement2.Value = list[1];
+            measurement2.Value = vocht;
             measurement2.TimeStamp = DateTime.Now;
             _context.Measurements.Add(measurement2);
+            Measurement measurement3 = new Measurement();
+            measurement3.BoxID = box.BoxID;
+            measurement3.SensorID = 12;
+            measurement3.Value = tempB;
+            measurement3.TimeStamp = DateTime.Now;
+            _context.Measurements.Add(measurement3);
+            Measurement measurement4 = new Measurement();
+            measurement4.BoxID = box.BoxID;
+            measurement4.SensorID = 11;
+            measurement4.Value = bVocht;
+            measurement4.TimeStamp = DateTime.Now;
+            _context.Measurements.Add(measurement4);
+            Measurement measurement5 = new Measurement();
+            measurement5.BoxID = box.BoxID;
+            measurement5.SensorID = 12;
+            measurement5.Value = temp;
+            measurement5.TimeStamp = DateTime.Now;
+            _context.Measurements.Add(measurement5);
             await _context.SaveChangesAsync();
 
         }
