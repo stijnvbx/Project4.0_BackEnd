716b013357fcd62f6774cfd2c0775d5f42215a8c
Stijn Van Braband <r0740656@student.thomasmore.be>
2021-02-01 10:41:12 +0100

    Encryption fix

--- Project4.0_BackEnd/Models/User.cs
+++ Project4.0_BackEnd/Models/User.cs
@@ -18,6 +18,8 @@ namespace Project4._0_BackEnd.Models
         public string PostalCode { get; set; }
         public string City { get; set; }
 
+        [NotMapped]
+        public string Token { get; set; }
 
         //Relations
         public int UserTypeID { get; set; }
---
+++ Project4.0_BackEnd/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Project4._0_BackEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project4._0_BackEnd.Services
+{
+    public interface IUserService
+    {
+        User Authenticate(string email, string password);
+    }
+}
