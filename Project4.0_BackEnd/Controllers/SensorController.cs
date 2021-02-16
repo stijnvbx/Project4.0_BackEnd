180ea1991e9bd85cb3d6ce07691a5e0eebfe2c3b
Stijn Van Braband <r0740656@student.thomasmore.be>
2021-02-16 09:44:51 +0100

    fix djorven

--- Project4.0_BackEnd/Controllers/SensorController.cs
+++ Project4.0_BackEnd/Controllers/SensorController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -27,8 +28,9 @@ namespace Project4._0_BackEnd.Controllers
         {
             return await _context.Sensors.ToListAsync();
         }
-
+*/
         // GET: api/Sensor/5
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<Sensor>> GetSensor(int id)
         {
@@ -42,7 +44,7 @@ namespace Project4._0_BackEnd.Controllers
             return sensor;
         }
 
-        // PUT: api/Sensor/5
+/*        // PUT: api/Sensor/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
